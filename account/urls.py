6ee587ef5f13bf25e6710c695b6617bd340b0cb1
LorenzCoder <136112857+LorenzCoder@users.noreply.github.com>
2025-04-10 06:04:36 +0200

    Logout Funktion hinzugefügt

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     # Auth
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from board import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/', include('board.urls')),
     path('account/', include('account.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
