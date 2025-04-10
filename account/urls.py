f4cdaa6cd1420e94b36cf31e93c4ae426235fb35
Marzy <145050460+MarzyCoder@users.noreply.github.com>
2025-04-10 11:34:10 +0200

    add logout url in accounts Urls

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # Auth
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
